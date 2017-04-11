8f850ec2e0925926c230d243c6f6c0b715793c81
BME\ifbpmurcia.externos <ifbpmurcia.externos@infobolsa.es>
2017-04-11 14:35:56 +0200

    Read API keys from gradle.properties

--- app/src/main/java/pablomurcia/evernoteclient/EvernoteAPIClientApp.java
+++ app/src/main/java/pablomurcia/evernoteclient/EvernoteAPIClientApp.java
@@ -9,8 +9,8 @@ import com.evernote.client.android.EvernoteSession;
  */
 
 public class EvernoteAPIClientApp extends Application {
-    private static final String CONSUMER_KEY = "pmurciao14";
-    private static final String CONSUMER_SECRET = "a2d2f6af9d8833ad";
+    private static final String CONSUMER_KEY = BuildConfig.EVERNOTE_CONSUMER_KEY;
+    private static final String CONSUMER_SECRET = BuildConfig.EVERNOTE_CONSUMER_SECRET;
     private static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.SANDBOX;
     private static final boolean SUPPORT_APP_LINKED_NOTEBOOKS = true;
 
