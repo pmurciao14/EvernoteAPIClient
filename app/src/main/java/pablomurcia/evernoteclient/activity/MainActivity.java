7667688936efe00907aba505f6006b2b915ba243
BME\ifbpmurcia.externos <ifbpmurcia.externos@infobolsa.es>
2017-04-11 15:28:37 +0200

    Get user notebooks

--- app/src/main/java/pablomurcia/evernoteclient/activity/MainActivity.java
+++ app/src/main/java/pablomurcia/evernoteclient/activity/MainActivity.java
@@ -8,6 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.evernote.client.android.EvernoteSession;
+import com.evernote.client.android.asyncclient.EvernoteCallback;
+import com.evernote.client.android.asyncclient.EvernoteNoteStoreClient;
+import com.evernote.edam.type.Notebook;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pablomurcia.evernoteclient.R;
 
@@ -31,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        getNotebooks();
+    }
+
+    private void getNotebooks(){
+        EvernoteNoteStoreClient noteStoreClient = EvernoteSession.getInstance().getEvernoteClientFactory().getNoteStoreClient();
+        noteStoreClient.listNotebooksAsync(new EvernoteCallback<List<Notebook>>() {
+            @Override
+            public void onSuccess(List<Notebook> result) {
+                List<String> namesList = new ArrayList<>(result.size());
+                for (Notebook notebook : result) {
+                    namesList.add(notebook.getName());
+                }
+            }
+
+            @Override
+            public void onException(Exception exception) {
+                Snackbar.make(null, getString(R.string.snackbar_get_notebooks_error), Snackbar.LENGTH_LONG).show();
             }
         });
     }
